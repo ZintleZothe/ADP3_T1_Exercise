bf3bbf2555bfdb4e1520743d2547b9d31ea064d1
ZintleZothe <216130565@mycput.ac.za>
2022-02-05 19:34:17 +0200

    Initial commit

---
+++ src/main/java/za/ac/cput/GroupApplication.java
@@ -0,0 +1,17 @@
+package za.ac.cput;
+
+
+public class GroupApplication {
+    /*
+    * Group List
+    * 1. Siyamthanda Tonjeni
+    * 2. Hlombekazi Mbelu
+    * 3. Zintle Zothe
+    * 4. Abongile Tsopi
+     */
+
+    public static  void main(String[] args) {
+
+        System.out.println("Welcome back !");
+    }
+}
